86ed4dbefe0ede0b092d1ee964710c6c396933a0
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-15 19:01:56 -0500

    sg main corchete

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Ubuntu-PinFinal" {
   ami           = "ami-09e67e426f25ce0d7"  
   instance_type = "t2.micro"
-  vpc_security_group_ids = aws_security_group.pinFinalSG.id
+  vpc_security_group_ids = [aws_security_group.pinFinalSG.id]
 
   user_data = "${file("instalacion-programas.sh")}"
 
