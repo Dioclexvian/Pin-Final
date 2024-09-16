829e96153237372b6c6db3ceb82383b4549bb604
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-15 19:25:59 -0500

    otra instancia

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "Ubuntu-PinFinal" {
+resource "aws_instance" "Ubuntu-PinFinal1" {
   ami           = "ami-09e67e426f25ce0d7"  
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.pinFinalSG.id]
