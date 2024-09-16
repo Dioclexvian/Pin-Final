475903357464e7a5f43c6da4183193212fb4b03d
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-16 18:45:16 -0500

    sin llave

--- setup.tf
+++ setup.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "pinFinalSG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # acceso generl
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   egress {
