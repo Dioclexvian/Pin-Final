e6059877e44ea3d380afe30028ea0763cee62e29
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-15 19:28:23 -0500

    otro output1

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "IpPublica-UbuntuServerEC2" {
   description = "La dirección IP pública de la instancia EC2: "
-  value       = aws_instance.Ubuntu-PinFinal.public_ip
+  value       = aws_instance.Ubuntu-PinFinal1.public_ip
 }
