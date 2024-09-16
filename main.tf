db8df478ec1f7dacbb5c175b54c051f5e4135cb1
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-15 19:15:39 -0500

    llave pem

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Ubuntu-PinFinal" {
   ami           = "ami-09e67e426f25ce0d7"  
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.pinFinalSG.id]
-
+  key_name = "llave_ssh"
   user_data = "${file("instalacion-programas.sh")}"
 
   tags = {
