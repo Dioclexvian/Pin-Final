6dc5009d3d880bd7712a324e95a5ffebc261f468
Daniel Castañeda <158247977+danskt9@users.noreply.github.com>
2024-09-16 19:21:16 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  type        = string
+  description = "Nombre general para los recursos"
+  default     = ""
+}
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR usado por la VPC"
+  default     = ""
+}
+variable "subnet_CIDR" {
+  type        = list(string)
+  description = "CIDR de los subnets"
+  default     = []
+
+}
