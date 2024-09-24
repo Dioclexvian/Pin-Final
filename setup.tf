e074604768c3a02916fa6f10dab2f3d40ec32f92
Ricardus22 <ricardo.aramayo@hotmail.com>
2024-09-24 19:30:54 -0300

    modif ingress y egress

--- setup.tf
+++ setup.tf
@@ -25,14 +25,14 @@ resource "aws_security_group" "pinFinalSG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]  
+    cidr_blocks = ["0.0.0.0/0"]  
   }
   egress  {
     description = "servicio web"
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   # egress {
   #   from_port   = 0
