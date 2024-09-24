21a632c026e401625a13d380f836678e57ea1aca
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-23 21:24:56 -0500

    prueba 2.0

--- setup.tf
+++ setup.tf
@@ -25,21 +25,21 @@ resource "aws_security_group" "pinFinalSG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["10.0.0.0/24"]  
   }
-  ingress  {
-    description = "trafico web"
+  egress  {
+    description = "servicio web"
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
   tags = {
     Name = "PinFinal-SG"
   }
