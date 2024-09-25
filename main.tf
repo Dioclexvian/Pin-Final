2295b3e4fe994db7c266f571b54396d3842e59d6
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-24 20:35:05 -0500

    prueba 3.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Ubuntu-PinFinal" {
-  ami           = "ami-09e67e426f25ce0d7"  
+  ami           = "ami-0a0e5d9c7acc336f1"  
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.pinFinalSG.id]
   # key_name = "llave_ssh"
