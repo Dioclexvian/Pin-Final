7587b9fb2e6fb5450478bb5e27f89adcf7a2dfb8
dioclexvian <dfsuarezs.ing@gmail.com>
2024-09-30 20:49:46 -0500

    prueba 5

--- network.tf
+++ network.tf
@@ -12,6 +12,8 @@ resource "aws_vpc" "vpc" {
 
 #Subnet 0 es Publica
 #Creaciòn de las subnet
+#¿aquí las subnetes deberían estar en zonas a y b, diferentes?
+#creo que debemos separarlos
 resource "aws_subnet" "subnets" {
   count      = 3
   vpc_id     = aws_vpc.vpc.id
@@ -22,9 +24,8 @@ resource "aws_subnet" "subnets" {
 }
 
 #Internet GW Publica subnet publica
-
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id  #attach con el igw?
   tags = {
     Name = "${var.name}-igw"
   }
@@ -40,8 +41,8 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0" #destino está bien así
+  gateway_id             = aws_internet_gateway.igw.id #este es el target?
 }
 
 resource "aws_route_table_association" "public_route_table" {
@@ -51,42 +52,43 @@ resource "aws_route_table_association" "public_route_table" {
 
 
 
-#Elastic IP para el NAT GW
+# #Elastic IP para el NAT GW
 
-resource "aws_eip" "nat_eip" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.igw]
-}
+# resource "aws_eip" "nat_eip" {
+#   domain     = "vpc"
+#   depends_on = [aws_internet_gateway.igw]
+# }
 
-# NAT Gateway
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.subnets[0].id
-  depends_on    = [aws_internet_gateway.igw]
+# # NAT Gateway
+# #necesitamos nat gw? 
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.subnets[0].id #la subnet debería ser la privada?
+#   depends_on    = [aws_internet_gateway.igw]
 
-  tags = {
-    Name = "${var.name}-nat"
-  }
-}
+#   tags = {
+#     Name = "${var.name}-nat"
+#   }
+# }
 
-resource "aws_route_table" "private_route_table" {
-  count  = 2
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table" {
+#   count  = 2
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "${var.name}-private-rt-${count.index + 1}"
-  }
-}
+#   tags = {
+#     Name = "${var.name}-private-rt-${count.index + 1}"
+#   }
+# }
 
-resource "aws_route" "private_route" {
-  count                  = 2
-  route_table_id         = aws_route_table.private_route_table[count.index].id
-  destination_cidr_block = "10.0.1.0/24"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
+# resource "aws_route" "private_route" {
+#   count                  = 2
+#   route_table_id         = aws_route_table.private_route_table[count.index].id
+#   destination_cidr_block = "10.0.1.0/24"
+#   nat_gateway_id         = aws_nat_gateway.nat.id
+# }
 
-resource "aws_route_table_association" "private_route_table" {
-  count          = 2
-  subnet_id      = aws_subnet.subnets[count.index + 1].id
-  route_table_id = aws_route_table.private_route_table[count.index].id
-}
+# resource "aws_route_table_association" "private_route_table" {
+#   count          = 2
+#   subnet_id      = aws_subnet.subnets[count.index + 1].id
+#   route_table_id = aws_route_table.private_route_table[count.index].id
+# }
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,5 +3,5 @@ vpc_cidr = "10.0.0.0/22"
 subnet_CIDR = [
   "10.0.0.0/24", # Publica
   "10.0.1.0/24", # private
-  "10.0.2.0/24"  # private
+  "10.0.2.0/24"  # private usamos una sola subnet? 10.0.1.0/23 ? 
 ]
