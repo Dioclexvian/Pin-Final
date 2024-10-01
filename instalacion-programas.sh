74e8473a1a7fc84cfd15c54e200e41e1650482a4
DioclexvianMacUD <dfsuarezs.ing@gmail.com>
2024-10-01 14:59:37 -0500

    prueba 5.2

--- instalacion-programas.sh
+++ instalacion-programas.sh
@@ -3,11 +3,11 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install awscli -y
 sudo apt install curl -y
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 #curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
+sudo apt install vlc -y
 
