7012497debc9ceae349907bfc020581b6ef97d8d
Ricardus22 <ricardo.aramayo@hotmail.com>
2024-10-08 11:28:04 -0300

    eks cluster en sh

--- instalacionProgramas.sh
+++ instalacionProgramas.sh
@@ -6,9 +6,31 @@ sudo apt install curl -y
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo apt install vlc -y
+# sudo apt install vlc -y
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+# Instalar eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+# Crear cluster EKS
+eksctl create cluster \
+--name eks-pin-final-1 \
+--region us-east-1 \
+--node-type t3.small \
+--nodes 3 \
+--with-oidc \
+--ssh-access \
+--ssh-public-key pin \
+--managed \
+--full-ecr-access \
+--zones us-east-1a,us-east-1b,us-east-1c
+
+# Desplegar pod de nginx
+kubectl create deployment nginx --image=nginx
+kubectl expose deployment nginx --port=80 --type=LoadBalancer
--- nginx_deployment.tf
+++ nginx_deployment.tf
@@ -1,53 +1,53 @@
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name = "nginx"
-  }
+# resource "kubernetes_deployment" "nginx" {
+#   metadata {
+#     name = "nginx"
+#   }
 
-  spec {
-    replicas = 1
+#   spec {
+#     replicas = 1
 
-    selector {
-      match_labels = {
-        app = "nginx"
-      }
-    }
+#     selector {
+#       match_labels = {
+#         app = "nginx"
+#       }
+#     }
 
-    template {
-      metadata {
-        labels = {
-          app = "nginx"
-        }
-      }
+#     template {
+#       metadata {
+#         labels = {
+#           app = "nginx"
+#         }
+#       }
 
-      spec {
-        container {
-          image = "nginx:latest"
-          name  = "nginx"
+#       spec {
+#         container {
+#           image = "nginx:latest"
+#           name  = "nginx"
 
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-}
+#           port {
+#             container_port = 80
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
-resource "kubernetes_service" "nginx_service" {
-  metadata {
-    name = "nginx-service"
-  }
+# resource "kubernetes_service" "nginx_service" {
+#   metadata {
+#     name = "nginx-service"
+#   }
 
-  spec {
-    selector = {
-      app = kubernetes_deployment.nginx.spec.0.template.0.metadata.0.labels.app
-    }
+#   spec {
+#     selector = {
+#       app = kubernetes_deployment.nginx.spec.0.template.0.metadata.0.labels.app
+#     }
 
-    port {
-      port        = 80
-      target_port = 80
-    }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
 
-    type = "LoadBalancer"
-  }
-}
+#     type = "LoadBalancer"
+#   }
+# }
