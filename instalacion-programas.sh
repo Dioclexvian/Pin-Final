0e71289ee57c50620cd7fce7a1ece3c40da798e0
DioclexvianMacUD <dfsuarezs.ing@gmail.com>
2024-10-10 07:50:26 -0500

    macbook

--- instalacion-programas.sh
+++ instalacion-programas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install awscli -y
+sudo apt update -y 
+#sudo apt install awscli -y
 sudo apt install curl -y
 #curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 #sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
