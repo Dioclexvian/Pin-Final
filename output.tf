3a7dc7c9f199fa558db8102f9453464cb3cd9aee
Ricardus22 <ricardo.aramayo@hotmail.com>
2024-10-13 12:58:55 -0300

    comment output file

--- output.tf
+++ output.tf
@@ -11,22 +11,22 @@ output "key_name" {
   value = aws_instance.Ubuntu-PinFinal.key_name
 }
 
-output "instance_public_ip" {
-  value = aws_instance.eks_instance.public_ip
-}
+# output "instance_public_ip" {
+#   value = aws_instance.eks_instance.public_ip
+# }
 
-output "private_key_path" {
-  value = local_file.private_key.filename
-}
+# output "private_key_path" {
+#   value = local_file.private_key.filename
+# }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
 
-output "public_subnet_id" {
-  value = aws_subnet.subnets[0].id
-}
+# output "public_subnet_id" {
+#   value = aws_subnet.subnets[0].id
+# }
 
-output "private_subnet_ids" {
-  value = [aws_subnet.subnets[1].id, aws_subnet.subnets[2].id]
-}
+# output "private_subnet_ids" {
+#   value = [aws_subnet.subnets[1].id, aws_subnet.subnets[2].id]
+# }
