8b3305012c37ba764e7e3eb0a077c63de2b6b273
dioclexvian <dfsuarezs.ing@gmail.com>
2024-10-14 20:15:57 -0500

    Final proyecto script completo instalación grafana y prometheus

--- eksInstall.sh
+++ eksInstall.sh
@@ -32,13 +32,87 @@ curl http://<ip>
 git clone https://github.com/kubernetes-sigs/aws-ebs-csi-driver.git
 cd aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/ecr
 kubectl apply -k .
+# ksctl create addon --name aws-ebs-csi-driver --cluster eksMundosE --region us-east-1
+
+#ejecuta permisos ebs
+#IAM Role
+eksctl create iamserviceaccount \
+          --name ebs-csi-controller-sa \
+          --namespace kube-system \
+          --cluster eksMundosE \
+          --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
+          --approve \
+          --override-existing-serviceaccounts \
+          --region us-east-1  
+
+        # Instalar EBS CSI Driver
+        kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=m>
+        # Patch EBS CSI Driver para que use el Service Account
+        kubectl patch deployment ebs-csi-controller \
+          -n kube-system \
+          --type=json \
+          -p="[{\"op\": \"replace\", \"path\": \"/spec/template/spec/serviceAccount\", \"value\": \"ebs-csi-cont>
+
 
 kubectl get pods -n kube-system -l app=ebs-csi-controller
 
-prometeus
 
+#########################################################
+#################prometeus################
+##########################################################
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 kubectl create namespace prometheus
 helm install prometheus prometheus-community/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp2" --set server.persistentVolume.storageClass="gp2"
 
+kubectl port-forward -n prometheus deploy/prometheus-server 9090 --address 0.0.0.0 
+#expone el servicio del prometheus al puerto con la ip del EC2
+
+
+# #crear el yaml   vim pv-manual.yaml
+# apiVersion: v1
+# kind: PersistentVolume
+# metadata:
+#   name: prometheus-pv
+# spec:
+#   capacity:
+#     storage: 30Gi
+#   accessModes:
+#     - ReadWriteOnce
+#   persistentVolumeReclaimPolicy: Retain
+#   csi:
+#     driver: ebs.csi.aws.com
+#     volumeHandle: vol-0263e1288eced70cd
+#   storageClassName: manual
+
+# kubectl apply -f pv-manual.yaml
+
+# kubectl delete pvc prometheus-server -n prometheus
+
+
+## crear otro archivo de config del prometeus - server
+# vim proServ.yaml
+# apiVersion: v1
+# kind: PersistentVolumeClaim
+# metadata:
+#   name: prometheus-server
+#   namespace: prometheus
+# spec:
+#   accessModes:
+#     - ReadWriteOnce
+#   resources:
+#     requests:
+#       storage: 8Gi
+#   storageClassName: manual  
+
+
+###########################################
+################grafana####################
+###########################################
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm repo list
+kubectl create namespace monitoring
+helm search repo grafana/grafana
+helm install grafana grafana/grafana --namespace monitoring --set persistence.storageClassName="gp2" --set persistence.enabled=true --set adminPassword='Cambiam3' --set service.type=LoadBalancer
